80f158c5e770d3c2f2356bb627a617597f2ff315
liuximing <459794155@qq.com>
2019-02-22 15:58:33 +0800

    提交关于页面的提交系统

--- app/components/OrderList/Item/index.jsx
+++ app/components/OrderList/Item/index.jsx
@@ -6,6 +6,9 @@ class Item extends React.Component{
     constructor(props, context) {
         super(props, context);
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+        this.state ={
+            commentState:2 // 0未评价 1评价中 2已评价
+        }
     }
     render(){
         const data = this.props.data
@@ -15,16 +18,77 @@ class Item extends React.Component{
                     <img src={data.img}/>
                 </div>
                 <div className="order-item-comment float-right">
-                    <button>评价</button>
-                </div>
+                        {
+                            this.state.commentState === 0
+                            // 未评价
+                            ? <button className="btn" onClick={this.showComment.bind(this)}>评价</button>
+                            :
+                                this.state.commentState === 1
+                                // 评价中
+                                ? ''
+                                // 已经评价
+                                : <button className="btn unseleted-btn">已评价</button>
+                        }
+                    </div>
                 <div className="order-item-content">
                     <span>商户：{data.title}</span>
                     <span>数量：{data.count}</span>
                     <span>价格：￥{data.price}</span>
                 </div>
+                {
+                    // “评价中”才会显示输入框
+                    this.state.commentState === 1
+                    ? <div className="comment-text-container">
+                        <textarea style={{width: '100%', height: '80px'}} className="comment-text" ref="commentText"></textarea>
+                        <button className="btn" onClick={this.submitComment.bind(this)}>提交</button>
+                        &nbsp;
+                        <button className="btn unseleted-btn" onClick={this.hideComment.bind(this)}>取消</button>
+                    </div>
+                    : ''
+                }
             </div>
         )
+    }
+    componentDidMount(){
+        this.setState({
+            commentState:this.props.data.commentState
+        })
+    }
+    showComment() {
+        // 显示输入框
+        this.setState({
+            commentState: 1
+        })
+    }
+    submitComment() {
+        // 获取操作函数
+        const submitComment = this.props.submitComment
+        // 获取id
+        const id = this.props.data.id
+        // 获取评价内容
+        const commentText = this.refs.commentText
+        const value = commentText.value.trim()
+        if (!value) {
+            return
+        }
+        // 执行数据提交
+        submitComment(id, value, this.commentOk.bind(this))
+    }
+    commentOk() {
+        // 已经评价，修改状态
+        this.setState({
+            commentState: 2
+        })
+    }
+    hideComment() {
+        // 隐藏输入框
+        this.setState({
+            commentState: 0
+        })
     }
 }
 
 export default Item
+
+
+
